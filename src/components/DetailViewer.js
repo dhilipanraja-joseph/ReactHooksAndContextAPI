7ffc670128151ffb145985657339bad7d09a69c7
dhilipanraja-joseph <dlipnraj@gmail.com>
2019-07-04 20:03:03 -0500

    react functional components

--- src/components/DetailViewer.js
+++ src/components/DetailViewer.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class DetailViewer extends Component {
+const DetailViewer =(props)=> {
+    let { name, order, sprites}= props.item;
+    let front_default= '';
+    if (sprites) front_default= sprites.front_default; 
     
-    render() {
-        let { name, order, sprites}= this.props.item;
-        let front_default= '';
-        if (sprites) front_default= sprites.front_default; 
-        
-        return <div>
-            <h3>{name}</h3>
-            <p>Order: {order}</p>
-            <div>
-                <img src={front_default} alt="no_img"/>
-            </div>
+    return <div>
+        <h3>{name}</h3>
+        <p>Order: {order}</p>
+        <div>
+            <img src={front_default} alt="no_img"/>
         </div>
-    }
+    </div>
 }
 
 export default DetailViewer;
--- src/components/ListViewer.js
+++ src/components/ListViewer.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ListViewer extends Component {
-    render() {
-        let { list, onListClick }= this.props
+const ListViewer= (props)=> {
+    let { list, onListClick }= props
 
-        let lists= list.length ? list.map((e,i)=> <div key={i}>
-            <p className="listItem" onClick={()=> onListClick(e.name)}>{e.name}</p>
-        </div>): <div>
-            <p className="listItem" onClick={()=> onListClick('noResults')}>NoResults</p>
-        </div>
+    let lists= list.length ? list.map((e,i)=> <div key={i}>
+        <p className="listItem" onClick={()=> onListClick(e.name)}>{e.name}</p>
+    </div>): <div>
+        <p className="listItem" onClick={()=> onListClick('noResults')}>NoResults</p>
+    </div>
 
-        return <div>
-            {lists}
-        </div>
-    }
-} 
+    return <div>
+        {lists}
+    </div>
+}
+
 
 export default ListViewer;
